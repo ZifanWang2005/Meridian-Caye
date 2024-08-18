3795c38ec5f78e8c8208ffde42895c4273918008
ZifanWang2005 <zifanwang2005@gmail.com>
2024-08-18 11:27:47 -0400

    adding the website

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+const myImage = document.getElementById("fishImage");
+
+var imageList = [];
+imageList.push(["images/neon-gold-platy.webp", 
+    '<div id="fishFacts"><p>This is a neon gold calico platy, a popular aquarium fish. Other varieties of platies include</p><ul><li>Red wag</li><li>High fin</li><li>Dalmatian</li></ul></div>']);
+imageList.push(["images/zebra-angelfish.webp", 
+    '<div id="fishFacts"><p>This is a zebra angelfish, a popular aquarium fish. Other varieties of angelfish include</p><ul><li>Black Lace</li><li>Marble</li><li>Golden</li></ul></div>']);
+imageList.push(["images/zebra-pleco.jpeg", 
+    '<div id="fishFacts"><p>This is a zebra pleco (L46), a highly sought-after aquarium fish. Other species of plecos include</p><ul><li>Gold Nugget (L018)</li><li>Royal</li><li>Colombian blue-eyed</li></ul></div>']);
+    
+
+myImage.onclick = () => {
+    ChangeFish();
+};
+
+let index = 0;
+
+function ChangeFish() {
+    index += 1;
+    if (index == imageList.length) {
+        index = 0;
+    }
+    
+    myImage.src = imageList[index][0];
+    document.getElementById('fishFacts').innerHTML = imageList[index][1];
+};
+
+let myButton = document.getElementById("nameButton");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
