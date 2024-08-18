820adc1f394f4e5bab8a30416b8a0ad100b5b85e
ZifanWang2005 <zifanwang2005@gmail.com>
2024-08-18 12:14:04 -0400

    removed the customized greeting message

--- scripts/main.js
+++ scripts/main.js
@@ -24,27 +24,3 @@ function ChangeFish() {
     myImage.src = imageList[index][0];
     document.getElementById('fishFacts').innerHTML = imageList[index][1];
 };
-
-let myButton = document.getElementById("nameButton");
-let myHeading = document.querySelector("h1");
-
-function setUserName() {
-    const myName = prompt("Please enter your name.");
-    if (!myName) {
-        setUserName();
-    } else {
-        localStorage.setItem("name", myName);
-        myHeading.textContent = `Mozilla is cool, ${myName}`;
-    }
-}
-
-if (!localStorage.getItem("name")) {
-    setUserName();
-  } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
-}
-
-myButton.onclick = () => {
-    setUserName();
-};
